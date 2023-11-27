ed53513968228a3dd1c5b608843f7acb6b545ee9
RubiSanchezLoor <135172892+RubiSanchezLoor@users.noreply.github.com>
2023-11-27 03:02:28 +0100

    Update ImpactoEcologico.java

--- impactoAmbiental/src/ImpactoEcologico.java
+++ impactoAmbiental/src/ImpactoEcologico.java
@@ -2,4 +2,8 @@
 interface ImpactoEcologico {
     // Método para obtener el impacto ecológico
     double obtenerImpactoEcologico(double consumoEnergia, double emisionesCO2, int cantidadResiduos);
+    
 }
+$ GIT_ COMMITTER_DATE=" 2023-11-26
+    09:02:55" git commit --date="2023-11-25
+    09:02:55"
